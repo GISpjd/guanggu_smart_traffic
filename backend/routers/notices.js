284dac557d6b2248690ded661357ee13d2f89b62
GISpjd <3232606740@qq.com>
2024-04-15 18:44:01 +0800

    后端接口部分大致完成

--- backend/routers/notices.js
+++ backend/routers/notices.js
@@ -3,4 +3,116 @@ var express = require('express')
 var router = express.Router()
 
 const { getOne, getAll, exec } = require('../db')
+
+
+/**
+ * 获取(对应状态的)公告
+ * GET /notices
+ */
+router.get('/', async (req, res) => {
+    try {
+        const { page = 1, size = 10, status } = req.query
+        const offset = (page - 1) * size
+        let sql
+        let data
+        let total
+        if (status) {
+            sql = `select * from event_notice where notice_status = ? order by id limit ?,?`
+            data = await getAll(sql, [status, offset, size])
+            sql = `select count(*) as total from event_notice where notice_status = ?`
+            total = await getOne(sql, [status])
+            res.send({
+                code: 0,
+                message: '获取对应状态的公告成功',
+                total,
+                data
+            })
+        } else {
+            sql = `select * from event_notice`
+            data = await getAll(sql)
+            sql = `select count(*) as total from event_notice`
+            total = await getOne(sql)
+            res.send({
+                code: 0,
+                message: '获取所有公告成功',
+                total,
+                data
+            })
+        }
+    } catch (error) {
+        res.status(500).send({
+            code: 500,
+            message: '服务器错误',
+            error: error.message
+        });
+    }
+
+})
+
+
+/**
+ * 获取单个公告信息
+ * GET /notices/:id
+ */
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        let sql = `select * from event_notice where id=?`
+        let data = await getOne(sql, [id])
+        res.send({
+            code: 0,
+            message: '获取指定id的公告成功',
+            data
+        })
+
+    } catch (error) {
+        res.status(500).send({
+            code: 500,
+            message: '服务器错误',
+            error: error.message
+        });
+    }
+})
+
+/**
+ * 发布公告
+ * POST /notices
+ */
+router.post('/', async (req, res) => {
+    try {
+        const { user_id, notice_title, notice_content } = req.body
+        const publish_time = new Date().toLocaleDateString()
+        let sql = `insert into event_notice(user_id, notice_title, notice_content, publish_time) values(?,?,?,?)`
+        const data = await exec(sql, [user_id, notice_title, notice_content, publish_time])
+        res.send({
+            code: 0,
+            message: '发布公告成功',
+            result: data,
+        })
+    } catch (error) {
+        res.status(500).send({
+            code: 500,
+            message: '服务器错误',
+            error: error.message
+        });
+    }
+})
+
+/**
+ * 更新相应公告状态
+ * PUT /notices/:id
+ */
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const { notice_status } = req.body
+    let sql = `update event_notice set notice_status=? where id=?`
+    const data = await exec(sql, [notice_status, id])
+    res.send({
+        code: 0,
+        message: '相应公告状态更新成功',
+        data
+    })
+})
+
+
 module.exports = router
